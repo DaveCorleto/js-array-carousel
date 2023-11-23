62f148a87ff736ef1d92b9ebaffa42032d326c07
DaveCorleto <davide.corleto@fastwebnet.it>
2023-11-23 16:05:16 +0100

    feat const imgCard, activeImgCard and .next

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+// Vado a prendermi ogni elemento con classe img-card 
+
+const imgCard = document.getElementsByClassName('img-card');
+
+console.log (imgCard[0])
+
+// Setto la variabile che dovrà "comandare"
+
+let activeImgCard = 0;
+
+// comandi del bottone next 
+
+const next = document.querySelector('.next');
+
+next.addEventListener('click', function() {
+
+    
+
+
+}
+)
