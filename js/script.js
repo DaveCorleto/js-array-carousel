3fcf9f8d2e506a0714f9616185fd8ece11830d2d
DaveCorleto <davide.corleto@fastwebnet.it>
2023-11-23 17:15:14 +0100

    feat prev button

--- js/script.js
+++ js/script.js
@@ -15,7 +15,57 @@ const next = document.querySelector('.next');
 
 next.addEventListener('click', function() {
 
-    
+    if (activeImgCard < imgCard.length -1) {
+
+        // tolgo lA CLASSE ACTIVE 
+
+        imgCard [activeImgCard].classList.remove('active');
+
+        // Aumentiamo l'indice di imgCard 
+
+        activeImgCard++;
+
+        // Ora che siamo passati al dispatchEvent.img-card successivo gli aggiungiamo la classe .active 
+
+        imgCard [activeImgCard].classList.add('active');
+
+        // se sono alla fine del carousel nascondo il button con classe .next perchè sono arrivato a fine carousel 
+
+        if (activeImgCard === imgCard.length -1) {
+            next.classList.add('hidden');
+        }
+    }
+
+
+}
+)
+
+// comandi del bottone prev 
+
+const prev = document.querySelector('.prev');
+
+prev.addEventListener('click', function() {
+
+    if (activeImgCard > 0) {
+
+        // tolgo lA CLASSE ACTIVE 
+
+        imgCard [activeImgCard].classList.remove('active');
+
+        // diminuiamo l'indice di imgCard 
+
+        activeImgCard--;
+
+        // Ora che siamo passati al dispatchEvent.img-card precedente gli aggiungiamo la classe .active 
+
+        imgCard [activeImgCard].classList.add('active');
+
+        // se sono all'inizio del carousel nascondo il button con classe .next perchè sono arrivato a fine carousel 
+
+        if (activeImgCard === 0) {
+            next.classList.add('hidden');
+        }
+    }
 
 
 }
